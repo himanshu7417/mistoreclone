b6c294059dadca1abc817840167a9e00f015e803
himanshu <himanshurai07417@gmail.com>
2022-05-22 01:16:36 +0530

    adding slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import PreNavbar from './components/PreNavbar'
 import {BrowserRouter as Router} from 'react-router-dom'
+import data from './data/data.json'
+import Slider from './components/Slider';
+
+
 function App() {
   return (
     <Router>
      <PreNavbar/>
      <Navbar/>
+     {/* passing image banner as props */}
+     <Slider start = {data.banner.start} /> 
     </Router>
   );
 }
