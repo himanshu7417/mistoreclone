4532516b54866d0dd8cc5fb9b2addb9958e1b2a6
himanshu <himanshurai07417@gmail.com>
2022-05-22 00:25:49 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar';
 import PreNavbar from './components/PreNavbar'
+import {BrowserRouter as Router} from 'react-router-dom'
 function App() {
   return (
-    <>
+    <Router>
      <PreNavbar/>
-    </>
+     <Navbar/>
+    </Router>
   );
 }
 
