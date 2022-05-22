5689ba3167bb17eb7f08cc623493ac13635f654a
himanshu <himanshurai07417@gmail.com>
2022-05-22 14:42:07 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import PreNavbar from './components/PreNavbar'
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter,
+  Routes,
+  Route} from 'react-router-dom'
 import data from './data/data.json'
 import Slider from './components/Slider';
 import Offers from './components/Offers';
 import Heading from './components/Heading';
+import StarProducts from './components/StarProducts';
+import HotAccessoriesMenu  from "./components/HotAccessoriesMenu"
+import HotAccessories from "./components/HotAccessories"
+
 
 
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+    
      <PreNavbar/>
      <Navbar/>
      {/* passing image banner as props */}
      <Slider start = {data.banner.start} /> 
      <Offers offer = {data.offer}/>
      <Heading text ="STAR PRODUCTS"/>
-    </Router>
+     <StarProducts starProduct = {data.starProduct}/>
+     <Heading text ="Hot Accessories"/>
+     
+     <HotAccessoriesMenu />
+     <Routes>
+       <Route exact path="/music" element ={<HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />} />
+       
+       <Route exact path="/smartDevice" element={<HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />} />
+       
+       <Route exact path="/home" element={<HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />} />
+       
+       <Route exact path="/lifestyle" element={<HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />} />
+       
+       <Route exact path="/mobileAccessories" element={<HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />} />
+       
+       </Routes>
+       
+    </BrowserRouter>
 
   );
 }
