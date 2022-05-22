ec525f2c0f67feab949ae68fdb30b3986507dee5
himanshu <himanshurai07417@gmail.com>
2022-05-22 17:18:15 +0530

    banner added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import StarProducts from './components/StarProducts';
 import HotAccessoriesMenu  from "./components/HotAccessoriesMenu"
 import HotAccessories from "./components/HotAccessories"
 import ProductReviews from './components/ProductReviews';
+import Videos from './components/Videos';
+import Banner from './components/Banner';
 
 
 
